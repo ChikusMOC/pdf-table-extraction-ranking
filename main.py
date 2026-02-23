80caec3760e0ca8fc24e4a7b6cf061a1f55ab6b6
Francisco de Assis P A Santos <35563930+ChikusMOC@users.noreply.github.com>
2026-02-22 21:49:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from src.processor import process_tables
 from src.ranking import generate_ranking
 
 def main():
-    pdf_path = "input.pdf"
-    tables = extract_tables(pdf_path)
-    df_clean = process_tables(tables)
+    pdf_path = "analisar.pdf" # Path to the PDF file you want to analyze
+        pages = "180-642"     # Range of pages to extract; can be adjusted as needed
+    start_index = 35          # Starting index for processing tables; change if necessary
+    tables = extract_tables(pdf_path, pages)
+    df_clean = process_tables(tables, start_index)
     df_ranked = generate_ranking(df_clean)
 
-    df_ranked.to_csv("ranking_output.csv", index=False)
+    df_ranked.to_csv("ranking_output.csv", index=False) # Save the ranked output to a CSV file
 
 if __name__ == "__main__":
     main()
